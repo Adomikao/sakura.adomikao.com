b62ee2368cf2a9f20dcc9eb80270f7186ee19458
adomikao <adomikao@foxmail.com>
2019-02-26 15:00:11 +0800

    目录修改

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuyongjian
+ * Date: 2019/2/26
+ * Time: 14:12
+ */
+
+
+// 绝对路径PATH
+
+define('ROOT', __DIR__ . '/');
+define('CI_PATH', ROOT . '/system');
+define('SITE_PATH', ROOT . '/application/site');
+
+define('ENVIRONMENT', 'development');
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuyongjian
+ * Date: 2019/2/26
+ * Time: 14:56
+ */
+
+include 'config.php';
+include SITE_PATH . '/index.php';
