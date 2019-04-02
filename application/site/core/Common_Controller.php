824ea965165b642f26c481e8f2f3e2334469cb5a
adomikao <adomikao@foxmail.com>
2019-04-02 10:57:31 +0800

    Update Common_Controller.php

--- application/site/core/Common_Controller.php
+++ application/site/core/Common_Controller.php
@@ -1,20 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yuyongjian
- * Date: 2019/2/27
- * Time: 11:24
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Common_Controller extends CI_Controller
 {
-    public $seoTitle = 'QAQ | Adomikao';
+    public $seoTitle = 'Sakura | Adomikao';
     public function __construct()
     {
         parent::__construct();
         $this->load->helper('uisite');
         $this->load->model('category_model', 'category');
         $this->load->model('image_model', 'image');
+        
+        ini_set('date.timezone','Asia/Shanghai');// 设置时区
+        
+        //隐藏X-Powered-By
+        if (function_exists('header_remove')) {
+            header_remove('X-Powered-By'); // PHP 5.3+
+        } else {
+            @ini_set('expose_php', 'off');
+        }
+
 
     }
-
 }
