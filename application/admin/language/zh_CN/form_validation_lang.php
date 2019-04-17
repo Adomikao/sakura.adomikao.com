f89c0e87587b9402bdddd5e3c056e4c16d3b7127
adomikao <adomikao@foxmail.com>
2019-04-17 13:30:25 +0800

    language package

---
+++ application/admin/language/zh_CN/form_validation_lang.php
@@ -0,0 +1,37 @@
+<?php
+
+$lang['required'] 			= "%s 必须填写.";
+$lang['isset']				= "%s 一定要有内容.";
+$lang['valid_email']		= "%s 必须是一个有效的电子邮箱地址.";
+$lang['valid_emails'] 		= "%s 必须是有效的电子邮箱地址.";
+$lang['valid_url'] 			= "%s 必须是有效的网址.";
+$lang['valid_ip'] 			= "%s 必须是一个有效IP地址.";
+$lang['min_length']			= "%s 至少包含 %s 个字.";
+$lang['max_length']			= "%s 不能超过 %s 个字.";
+$lang['exact_length']		= "%s 必须刚好 %s 个字.";
+$lang['alpha']				= "%s 只能包含英文字母.";
+$lang['alpha_numeric']		= "%s 只能包含英文字母或数字.";
+$lang['alpha_dash']			= "%s 只能包含英文字母、数字、下划线、或破折号.";
+$lang['numeric']			= "%s 只能包含数字.";
+$lang['is_numeric']			= "%s 只能包含数字.";
+$lang['integer']			= "%s 只能包含整数.";
+$lang['regex_match']		= "%s 格式不正确.";
+$lang['matches']			= "%s 与 %s 不相符合.";
+$lang['is_natural']			= "%s 必须是自然数(非负整数).";
+$lang['is_natural_no_zero']	= "%s 必须是大于零的自然数(非负整数).";
+$lang['notop']	= "产品必须归档至二级分类";
+$lang['timeUnique']	= "该日期已存在推荐";
+$lang['timeEditUnique']	= "该日期已存在推荐";
+$lang['timeEarly']	= "不能编辑未来推荐";
+$lang['forumUnique']	= "该帖子已存在";
+$lang['forumEditUnique']	= "该帖子已存在";
+$lang['phone_check']	= "手机号已被使用";
+$lang['phoneEdit_check']	= "手机号已被使用";
+$lang['email_check']	= "邮箱已被使用";
+$lang['emailEdit_check']	= "邮箱已被使用";
+$lang['forumtop']	= "帖子必须归档至二级分类";
+$lang['forumexist']	= "帖子ID无效";
+$lang['timeUnique']	= "该日期已存在文章";
+$lang['timeEditUnique']	= "该日期已存在其它文章";
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/chinesesimplified/form_validation_lang.php */
