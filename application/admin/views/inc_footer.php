4bec747fcf4d14f0c99c8c4eee3217fdd4098bb0
adomikao <adomikao@foxmail.com>
2019-04-30 11:38:24 +0800

    Create inc_footer.php

---
+++ application/admin/views/inc_footer.php
@@ -0,0 +1,15 @@
+				</div><!-- end body content content -->
+			</div> <!-- end body content -->
+		</div> <!-- end #body-body .container -->
+		<div class='clear' id="footer-push"></div>
+	</div> <!-- end #body-main -->
+
+	<!-- footer -->
+	<div id="body-footer">
+		<div class="container-fluid">
+			
+		</div>
+	</div>
+	<!-- end #body-footer -->
+</body>
+</html>
