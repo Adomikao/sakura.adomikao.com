6045f9ae09cb201bc18512e5ea475350c734eed2
adomikao <adomikao@gmail.com>
2022-05-12 22:15:07 +0800

    Update config.php

--- config.php
+++ config.php
@@ -21,7 +21,9 @@ define('STATIC_URL', GLOBAL_URL . 'public/static/');
 define('UPLOAD_URL', GLOBAL_URL . 'public/upload/');
 define('IMG_URL', STATIC_URL . 'img/');
 define('SAKURA_GITEE_URL','https://adomikao.gitee.io/media/sakura/');
+define('GITHUB_CDN_URL', 'https://cdn.jsdelivr.net/gh/adomikao/CDN/sakura');
 define('IS_GITEE',false);
+define('IS_GITHUB',false);
 
 # 引用绝对路径PATH定义
 define('ROOT', __DIR__ . '/');
@@ -32,6 +34,8 @@ define('SITE_PATH', ROOT . 'application/site');
 define('ADMIN_PATH', ROOT . 'application/admin');
 # 可忽略 当css|js改变时替换本地缓存,将false 替换为 'v[1,2...]'
 define('STATIC_V', 'v1');
+
+
 
 define('HMACPWD', 'SA1S2D3F4G5H6J7K8L9');
 
